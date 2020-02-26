c62f12b9ca343ad9dfe173fa1e532f13a4a85756
blinkofaneye <mratiarison41@gmail.com>
2020-02-26 14:36:03 +0300

    Add contact-create.json

--- resources/js/components/client/ContactCreateComponent.vue
+++ resources/js/components/client/ContactCreateComponent.vue
@@ -4,135 +4,48 @@
             <toolbar></toolbar>
 
             <v-container>
-                <h1>New Ticket</h1>
+                <h1>{{ contactCreate.title }}</h1>
                 <v-row>
                     <v-col cols=10 offset-md=1>
-                        <v-card class="pa-7" elevation=10 outlined>
-                            <h4 class="title pt-3">ADD A NEW TICKET</h4>
+                        <v-card class="pa-7" :elevation="contactCreate.content.elevation" outlined>
+                            <h4 class="title pt-3">{{ contactCreate.content.title }}</h4>
                             <v-form>
-                                <v-row class="nowrap-overflow">
+                                <v-row class="nowrap-overflow" v-for="(item, index) in contactCreate.content.fields" :key="index">
                                     <v-col cols=4 offset-md="1">
-                                        <span class="subtitle-1">First name :</span> 
+                                        <span class="subtitle-1" v-html="item.label"></span>
                                     </v-col>
-                                    <v-col cols=6> 
-                                        <v-text-field :value="ticket_form_data.firstname" dense outlined readonly />
-                                    </v-col>
-                                </v-row>
-
-                                <v-row class="nowrap-overflow">
-                                    <v-col cols=4 offset-md="1">
-                                        Last name :
-                                    </v-col>
-                                    <v-col cols=6> 
-                                        <v-text-field :value="ticket_form_data.lastname" dense outlined readonly />
-                                    </v-col>
-                                </v-row>
-
-                                <v-row class="nowrap-overflow">
-                                    <v-col cols=4 offset-md="1">
-                                        Email :
-                                    </v-col>
-                                    <v-col cols=6> 
-                                        <v-text-field :value="ticket_form_data.mail" dense outlined readonly />
-                                    </v-col>
-                                </v-row>
-
-                                <v-row class="nowrap-overflow">
-                                    <v-col cols=4 offset-md="1">
-                                        Company (<span class="red--text">*</span>) :
-                                    </v-col>
-                                    <v-col cols=6> 
-                                        <v-text-field :value="ticket_form_data.company" dense outlined required readonly />
-                                    </v-col>
-                                </v-row>
-
-                                <v-row class="nowrap-overflow">
-                                    <v-col cols=4 offset-md="1">
-                                        Subject (<span class="red--text">*</span>) :
-                                    </v-col>
-                                    <v-col cols=6> 
-                                        <v-text-field v-model="ticket_form_data.subject" dense outlined required name="subject" @change="handleOnChangeEvent"/>
-                                    </v-col>
-                                </v-row>
-
-                                <v-row class="nowrap-overflow">
-                                    <v-col cols=4 offset-md="1">
-                                        Additional Contacts <i>(Add additional contact emails here, separate multiple emails with a semi-colon or comma)</i>  :
-                                    </v-col>
-                                    <v-col cols=6> 
-                                        <v-text-field v-model="ticket_form_data.additional" dense outlined required name="additional" @change="handleOnChangeEvent"/>
-                                    </v-col>
-                                </v-row>
-
-                                <!-- Details field -->
-                                <v-row class="nowrap-overflow">
-                                    <v-col cols=4 offset-md="1">
-                                        Details (<span class="red--text">*</span>)
-                                    </v-col>
-                                    <v-col cols=6> 
+                                    <v-col cols=6>
+                                        <v-text-field v-if="item.type === 'textfield'" :value="ticket_form_data[item.model]" :required="item.required" :readonly="item.readonly" dense outlined @change="onChangeHandlers[item.onChangeHandler]" />
                                         <v-textarea
+                                            v-else-if="item.type === 'textarea'"
                                             outlined
-                                            dense 
-                                            required
-                                            name="details"
-                                            v-model="ticket_form_data.details"
-                                            @change="handleOnChangeEvent"></v-textarea>
-                                    </v-col>
-                                </v-row>
-
-                                <!-- Category field -->
-                                <v-row class="nowrap-overflow">
-                                    <v-col cols=4 offset-md="1">
-                                        Category (<span class="red--text">*</span>)
-                                    </v-col>
-                                    <v-col cols=6> 
-                                        <v-select
-                                            name="category"
-                                            outlined
-                                            required
-                                            dense 
-                                            v-model="ticket_form_data.category"
-                                            :items="categories_list"
-                                            @change="handleOnChangeEvent"></v-select>
-                                        </v-col>
-                                </v-row>
-
-                                <!-- Country field -->
-                                <v-row class="nowrap-overflow">
-                                    <v-col cols=4 offset-md="1">
-                                        Country (<span class="red--text">*</span>)
-                                    </v-col>
-                                    <v-col cols=6> 
-                                        <v-select
-                                            name="country"
-                                            outlined
-                                            required
                                             dense
-                                            v-model="ticket_form_data.country"
-                                            :items="countries_list"
-                                            @change="handleOnChangeEvent"></v-select>
-                                    </v-col>
-                                </v-row>
-                                <v-row>
-                                    <v-col cols=4 offset-md="1">
-                                        <b>Attach document</b> (If you receive a large file error please Zip your attachment)
-                                    </v-col>
-                                    <v-col cols=6> 
+                                            :required="item.required"
+                                            v-model="ticket_form_data[item.model]"
+                                            @change="onChangeHandlers[item.onChangeHandler]"></v-textarea>
+                                        <v-select
+                                            v-else-if="item.type === 'select'"
+                                            outlined
+                                            :required="item.required"
+                                            dense
+                                            v-model="ticket_form_data[item.model]"
+                                            :items="item.items"
+                                            @change="onChangeHandlers[item.onChangeHandler]"></v-select>
                                         <v-file-input
-                                            v-model="ticket_form_data.files"
-                                            color="deep-purple accent-4"
-                                            counter
-                                            multiple
+                                            v-else-if="item.type === 'file'"
+                                            v-model="ticket_form_data[item.model]"
+                                            :counter="item.counter"
+                                            :multiple="item.multiple"
                                             dense
                                             tile
-                                            style="z-index: 2000;"
-                                            placeholder="Select your files"
+                                            :style="item.style"
+                                            :placeholder="item.placeholder"
                                             prepend-icon=""
                                             outlined
-                                            :show-size="1000">
+                                            :show-size="item.showSize">
 
                                             <template v-slot:append>
-                                                    <v-icon>mdi-file-upload</v-icon>
+                                                <v-icon>{{ item.icon }}</v-icon>
                                             </template>
                                             <template v-slot:selection="{ index, text }">
                                                 <v-chip
@@ -150,21 +63,22 @@
                                         </v-file-input>
                                     </v-col>
                                 </v-row>
+
                                 <v-row>
                                     <v-col cols=8 offset-md=1>
-                                        <p>By submitting this request you understand and agree to Oracle <span class="red--text">Terms of Use</span> and <span class="red--text">Privacy Policy</span></p>
+                                        <p v-html="contactCreate.content.paragraph"></p>
                                     </v-col>
                                 </v-row>
                                 <v-row>
                                     <v-col cols=2 offset-md=10>
-                                        <v-btn tile color="success" @click="submitTicketForm">Submit</v-btn>
+                                        <v-btn tile :color="contactCreate.content.button.color" @click="submitTicketForm">{{ contactCreate.content.button.label }}</v-btn>
                                     </v-col>
                                 </v-row>
                             </v-form>
                         </v-card>
                     </v-col>
                 </v-row>
-            </v-container>    
+            </v-container>
 
             <footer :class="init_data.footer.class" :style="'background:' + init_data.footer.style.bgcolor" >
                 <div class="secpage">
@@ -223,6 +137,7 @@ export default {
   data: () => ({
     offsetTop: 0,
     topbarStyle: "background:#fff; padding:0px 130px; border-color:#fff;",
+    contactCreate: {content: {}},
     ticket_form_data: {
         firstname: "",
         lastname: "",
@@ -238,12 +153,20 @@ export default {
     categories_list: [],
     countries_list: [],
     init_data: {},
-    contact_history_list: {}
+    contact_history_list: {},
+    onChangeHandlers: {}
   }),
   created() {
     axios.get('json/innerblank.json')
     .then(response => {
       this.init_data = response.data;
+    })
+    .catch(e => {
+      this.errors.push(e)
+    });
+    axios.get('json/contact-create.json')
+    .then(response => {
+      this.contactCreate = response.data;
     })
     .catch(e => {
       this.errors.push(e)
@@ -255,6 +178,11 @@ export default {
     this.ticket_form_data.mail = "example@example.com";
     this.ticket_form_data.company = "Ndegde Informatics Private Limited";
 
+    this.onChangeHandlers.subject = this.handleOnChangeEvent;
+    this.onChangeHandlers.additional = this.handleOnChangeEvent;
+    this.onChangeHandlers.details = this.handleOnChangeEvent;
+    this.onChangeHandlers.category = this.handleOnChangeEvent;
+    this.onChangeHandlers.country = this.handleOnChangeEvent;
   },
   methods: {
     submitTicketForm() {
