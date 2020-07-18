9887acba021336979d88a1253535681e98b616b3
Pratim Kanrar <kanrar.pratim@gmail.com>
2020-07-18 07:34:26 +0530

    Update IndexController.php

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -19,7 +19,7 @@ use Illuminate\Http\RedirectResponse;
 class IndexController extends Controller
 {
     public function index() {
-        return redirect('/client_main');
+        return redirect('/client_main/client_main_content');
     }
     public function gotoAdmin(Request $req) {
       if($req->session()->get('role')=='admin')
@@ -48,28 +48,28 @@ class IndexController extends Controller
     public function gotoLoginThree() {
         return view('loginthree');
     }
-    public function gotoMain() {
-        return view('main');
+    public function gotoMain(Request $request) {
+        return view('main')->with('fileName',$request->fileName);
     }
-    public function gotoInner() {
-        return view('inner');
+    public function gotoInner(Request $request) {
+        return view('inner')->with('fileName',$request->fileName);
     }
-    public function gotoInner2() {
-        return view('inner2');
+    public function gotoInner2(Request $request) {
+        return view('inner2')->with('fileName',$request->fileName);
     }
-    public function gotoInner3() {
-        return view('inner3');
+    public function gotoInner3(Request $request) {
+        return view('inner3')->with('fileName',$request->fileName);
     }
-    public function gotoInner4() {
-        return view('inner4');
-    }
-
-    public function gotoInnerblank() {
-        return view('innerblank');
+    public function gotoInner4(Request $request) {
+        return view('inner4')->with('fileName',$request->fileName);
     }
 
-    public function gotoInnerblank2() {
-        return view('innerblank2');
+    public function gotoInnerblank(Request $request) {
+        return view('innerblank')->with('fileName',$request->fileName);
+    }
+
+    public function gotoInnerblank2(Request $request) {
+        return view('innerblank2')->with('fileName',$request->fileName);
     }
 
     public function import_menu() {
